dd8a02ead8ced068291e7d2cd1d13e9b98dac6ea
Arup Panda <arupp@capitalnumbers.com>
2025-12-08 23:11:38 +0530

    Add CORS support and update package dependencies
    - Installed 'cors' package to handle Cross-Origin Resource Sharing.
    - Configured CORS in the Express app to allow requests from 'http://localhost:3000'.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import express from "express";
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import Route from "./routes/index.js";
-
+import cors from 'cors';
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000"}))
 
 // Routes
 app.use('/api/v1', Route);
